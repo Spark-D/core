8d22996aed1cc33edc98df084f0ae8aa6f3cb5bf
박슬기 <hangangnaru@plateer.com>
2025-04-07 15:11:07 +0900

    AppConfig 순수java -> Spring으로 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,23 +9,32 @@ import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+//구성(설정)정보 담당하는 파일이다
+@Configuration
 //공연기획자 역할
 public class AppConfig {
 
+    //스프링컨테이너에 빈을 등록하는 행위
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository()); //생성자 주입
     }
 
-    private static MemberRepository memberRepository() {
+    @Bean
+    public static MemberRepository memberRepository() {
         return new MemberMemoryRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private static DiscountPolicy discountPolicy() {
+    @Bean
+    public static DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,13 +3,21 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     //main method로 확인하는 것은 한계가 있다. 좋지 않은 방법. TDD를 활용하라
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        //ApplicationContext = 스프링컨테이너 이고, @Configuration 어노테이션이 붙은 AppConfig를 설정정보로 사용한다 
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //AppConfig에 @Bean이 붙은 메서드의 명을 스프링 빈의 이름으로 사용한다 (스프링빈)
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
         
