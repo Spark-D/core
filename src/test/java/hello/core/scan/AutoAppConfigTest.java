586e7124084b236324729726c52ad56a316c198b
박슬기 <hangangnaru@plateer.com>
2025-04-10 15:12:16 +0900

    컴포넌트 탐색 위치와 기본 스캔 대상

--- src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -3,6 +3,7 @@ package hello.core.scan;
 import hello.core.AutoAppConfig;
 import hello.core.member.MemberService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,6 +14,13 @@ public class AutoAppConfigTest {
     void basicScan() {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AutoAppConfig.class);
 
+        String[] beanDefinitionNames = context.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = context.getBeanDefinition(beanDefinitionName);
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                System.out.println("beanDefinitionName : "+ beanDefinitionName);
+            }
+        }
         MemberService memberService = context.getBean(MemberService.class);
         assertThat(memberService).isInstanceOf(MemberService.class);
 
