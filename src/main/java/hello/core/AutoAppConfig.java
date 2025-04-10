b1f7ae8f2dc8b0ee894f2e0dd74ba0d284e7d87d
박슬기 <hangangnaru@plateer.com>
2025-04-10 15:28:39 +0900

    컴포넌트 필터 어노테이션 만들어보기

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 @ComponentScan(
         // 컴포넌트 스캔을 시작할 패키지를 지정할수 있다, 지정하지 않으면 설정정보 클래스의 패키지가 컴포넌트 스캔의 베이스페키지가 된다(관례 사용 권장)
-        basePackages = "hello.core.member",
+//        basePackages = "hello.core.member",
         // @Configuration은 빼고 컴포넌트 스캔이 되도록 설정
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Configuration.class})
 )
---
+++ src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,36 @@
+package hello.core.scan.filter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ComponentFilterAppConfigTest {
+
+    @Test
+    void filterScan() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(FilterAppConfig.class);
+        BeanA beanA = context.getBean(BeanA.class);
+        assertNotNull(beanA);
+
+//        BeanB beanB = context.getBean(BeanB.class);
+        assertThrows(NoSuchBeanDefinitionException.class, () -> context.getBean(BeanB.class));
+
+    }
+
+    @Configuration
+    @ComponentScan(
+            includeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class), //type = FilterType.ANNOTATION은 생략가능
+            excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyExcludeComponent.class)
+    )
+    static class FilterAppConfig {
+    }
+}
