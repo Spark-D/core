8ae53fdb84c005f358c52a4977f25160147bb438
박슬기 <hangangnaru@plateer.com>
2025-05-19 15:16:36 +0900

    InitializingBean, DisposableBean 인터페이스 구현으로 빈등록초기화, 소멸 시점 확인

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,15 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출 , url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+
     }
 
     public void setUrl(String url) {
@@ -27,5 +29,19 @@ public class NetworkClient {
     public void disconnect() {
         System.out.println("close  : " + url);
 
+    }
+
+    //스프링이 의존관계를 끝내고나서 호출됨
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("afterPropertiesSet 호출");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("destroy 호출");
+        disconnect();
     }
 }
